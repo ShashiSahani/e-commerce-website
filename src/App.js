bddfb1657038e71ecebf05217fb7a9b175008eea
ShashiSahani <shashisahani496@gmail.com>
2024-03-03 00:24:21 +0530

    shopcategory added

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Shops />} />
           <Route path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
-          <Route path="/womens" element={<ShopCategory banner={women_banner} category="womens" />} />
-          <Route path="/kids" element={<ShopCategory  banner={kids_banner} category="kids" />} />
+          <Route path="/womens" element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path="/kids" element={<ShopCategory  banner={kids_banner} category="kid" />} />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,12 +1,35 @@
 import React, {  useContext } from 'react'
 import './CSS/ShopCategory.scss'
+import dropdownIcon from '../Assets/dropdown_icon.png'
 import { ShopContext } from '../components/Context/ShopContext'
+import Items from '../components/Items/Items'
 function ShopCategory(props) {
   const {all_product}=useContext(ShopContext)
-  console.log("all_product====>",all_product)
+  console.log(all_product)
   return (
     <div className='shop-category'>
-      <img src={props.banner} alt="" />
+      <img  className="shop-category-banner" src={props.banner} alt="banner" />
+      <div className="shopcategory-indexsort">
+        <p>
+          <span>Showing 1-12</span> out of {all_product.length} product
+        </p>
+        <div className='shopcategory-sort'>
+          Sort by <img src={dropdownIcon} alt=''/>
+
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {
+         all_product.map((item,i)=>{
+          if(props.category===item.category){
+            return <Items  key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+          }
+          else{
+            return null
+          }
+         }) 
+        }
+      </div>
     </div>
 
   )
