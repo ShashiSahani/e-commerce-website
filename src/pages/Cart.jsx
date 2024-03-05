b741d2f9a7ee79171d713112a0e139a731a92bae
ShashiSahani <shashisahani496@gmail.com>
2024-03-05 20:02:54 +0530

    add the cartItem functionality add to cart ,removetocart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import './CSS/Cart.scss'
+import CartItems from '../components/CartItems/CartItems'
 function Cart() {
   return (
-    <div className='cart'>
-      <h1>cart</h1>      
+    <div className='cartitem'>
+      <CartItems/>  
     </div>
   )
 }
