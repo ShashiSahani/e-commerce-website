e1d893e8e89ed30dcdf749c67a4907295002f0fc
ShashiSahani <shashisahani496@gmail.com>
2024-03-15 00:46:06 +0530

    add responsive login page

--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import './NewsLetter.scss'
-import *as  String from '../../Assets/Utils/Strings'
+import React from "react";
+import "./NewsLetter.scss";
+import * as String from "../../Assets/Utils/Strings";
 function NewsLetter() {
   return (
-    <div className='newsletter'>
+    <div className="newsletter">
       <h1>{String.Get_Exclusive_offers_on_your_Email}</h1>
       <p>{String.newsLetterp}</p>
       <div>
         <input type="email" name="" id="" placeholder={String.youremail} />
-      <button>{String.Subscribe}</button>
+        <button>{String.Subscribe}</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default NewsLetter
+export default NewsLetter;
--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,26 +1,82 @@
-import React from 'react'
-import './CSS/LoginSignup.scss';
-import * as String from '../Assets/Utils/Strings'
+import React, { useState } from "react";
+import "./CSS/LoginSignup.scss";
+import * as String from "../Assets/Utils/Strings";
 function LoginSignup() {
+  const [state, setState] = useState("Login");
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+  const changeHandler = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const login = async () => {
+    console.log("Login", formData);
+  };
+
+  const signup = async () => {
+    console.log("sign Up", formData);
+  };
+
   return (
-    <div className='loginsignup'>
+    <div className="loginsignup">
       <div className="loginsignup-container">
-        <h1>{String.sing_up} </h1>
+        <h1>{state} </h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your name' />
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
+          {state === "Sign Up" ? (
+            <input
+              type="text"
+              name="username"
+              placeholder="Your name"
+              value={formData.username}
+              onChange={changeHandler}
+            />
+          ) : null}
+          <input
+          name="email"
+            type="email"
+            placeholder="Email Address"
+            value={formData.email}
+            onChange={changeHandler}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={changeHandler}
+          />
         </div>
-        <button>{String.Continue}</button>
-        <p className='loginsignup-login'>Already have account ? <span>Login</span></p>
+        <button
+          onClick={() => {
+            state === "Login" ? login() : signup();
+          }}
+        >
+          {String.Continue}
+        </button>
+        {state === "Sign Up" ? (
+          <p className="loginsignup-login">
+            Already have account ?{" "}
+            <span onClick={() => setState("Login")}>Login</span>
+          </p>
+        ) : (
+          <p className="loginsignup-login">
+            Create an account ?{" "}
+            <span onClick={() => setState("Sign Up")}>Click here</span>
+          </p>
+        )}
         <div className="loginsignup-agree">
           <input type="checkbox" name="" id="" />
           <p>By continuing ,I agree to the terms of use & privacy policy.</p>
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default LoginSignup
+export default LoginSignup;
