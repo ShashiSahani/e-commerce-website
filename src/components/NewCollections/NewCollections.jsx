0afc350f65500c9dac68758b7a997e3d526625ea
ShashiSahani <shashisahani496@gmail.com>
2024-03-20 15:27:59 +0530

    fetching popular and newcollection data from backend

--- src/components/NewCollections/NewCollections.jsx
+++ src/components/NewCollections/NewCollections.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./NewCollections.scss";
-import new_collections from "../../Assets/new_collections";
+// import new_collections from "../../Assets/new_collections";
 import * as String from "../../Assets/Utils/Strings";
 import Items from "../Items/Items";
 function NewCollections() {
+  const [new_collection, setNew_Collection] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8000/newcollection")
+      .then((response) => response.json())
+      .then((data) => setNew_Collection(data));
+  }, []);
+
+  console.log("new_collection==>",new_collection)
   return (
     <div className="new-collections">
       <h1>{String.New_Collections}</h1>
       <hr />
       <div className="collections">
-        {new_collections.map((item, i) => {
-          return (
-            <Items
-              key={i}
-              id={item.id}
-              name={item.name}
-              image={item.image}
-              new_price={item.new_price}
-              old_price={item.old_price}
-            />
-          );
-        })}
+        {new_collection.length === 0 ? (
+          <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+            <p >No Product</p>
+          </div>
+        ) : (
+          new_collection?.map((item, i) => {
+            return (
+              <Items
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          })
+        )}
       </div>
     </div>
   );
--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Popular.scss";
 import data_product from "../../Assets/data";
 import Items from "../Items/Items";
-import * as String from '../../Assets/Utils/Strings';
+import * as String from "../../Assets/Utils/Strings";
 // console.log(data_product);
 function Popular() {
+  const [popularProducts, setPopularProduct] = useState([]);
+  //fetching the api data from backend 
+  useEffect(() => {
+    fetch("http://localhost:8000/popularinwomen")
+      .then((response) => response.json())
+      .then((data) => setPopularProduct(data));
+  }, []);
+
   return (
     <div className="popular">
-     <div className="popular-main">
-     <h1>{String.popular}</h1>
-      <hr />
-      <div className="popular-item">
-        {data_product?.map((item, i) => {
-          return (
-            <Items
-              key={i}
-              id={item.id}
-              name={item.name}
-              image={item.image}
-              new_price={item.new_price}
-              old_price={item.old_price}
-            />
-          );
-        })}
+      <div className="popular-main">
+        <h1>{String.popular}</h1>
+        <hr />
+        <div className="popular-item">
+          {popularProducts.length === 0 ? (
+            <div>
+              <p>No Popular Product Found!</p>
+            </div>
+          ) : (
+            popularProducts?.map((item, i) => {
+              return (
+                <Items
+                  key={i}
+                  id={item.id}
+                  name={item.name}
+                  image={item.image}
+                  new_price={item.new_price}
+                  old_price={item.old_price}
+                />
+              );
+            })
+          )}
+        </div>
       </div>
-     </div>
     </div>
   );
 }
