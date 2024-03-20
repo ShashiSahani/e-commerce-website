dc086c7baff61e215b8361604264c1812bef1d4c
ShashiSahani <shashisahani496@gmail.com>
2024-03-20 22:05:33 +0530

    api call for addtocart and removetocart and after logout /login data still remain in cart as it is

--- src/components/Context/ShopContext.jsx
+++ src/components/Context/ShopContext.jsx
@@ -9,50 +9,95 @@ const getDefaultCart = () => {
   return cart;
 };
 const ShopContextProvider = (props) => {
-  const [all_product,setAll_Product]=useState([]);
+  const [all_product, setAll_Product] = useState([]);
   const [cartItems, setCartItems] = useState(getDefaultCart);
-  const [totalItemCount,setTotalItemCount]=useState(0)
-// fetching the data from backend
-    useEffect(()=>
-    {
-      fetch('http://localhost:8000/allproducts')
-      .then((response)=>response.json())
-      .then((data)=>setAll_Product(data))
-    },[])
+  const [totalItemCount, setTotalItemCount] = useState(0);
+  // fetching the data from backend
+  useEffect(() => {
+    fetch("http://localhost:8000/allproducts")
+      .then((response) => response.json())
+      .then((data) => setAll_Product(data));
+
+    if (localStorage.getItem("auth-token")) {
+      fetch("http://localhost:8000/getcart", {
+        method: "POST",
+        headers: {
+          Accept: "Application/form-data",
+          "auth-token": `${localStorage.getItem("auth-token")}`,
+          "Content-Type": "application/json",
+        },
+        body: "",
+      })
+        .then((response) => response.json())
+        .then((data) => setCartItems(data));
+    }
+  }, []);
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-    console.log("okk",cartItems);
+    console.log("okk", cartItems);
+    if (localStorage.getItem("auth-token")) {
+      fetch("http://localhost:8000/addtocart", {
+        method: "POST",
+        headers: {
+          Accept: "application/form-data",
+          "auth-token": `${localStorage.getItem("auth-token")}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ itemId: itemId }),
+      })
+        .then((response) => response.json())
+        .then((data) => setCartItems(data));
+    }
   };
   const removeFromCart = (itemId) => {
+    // setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if (localStorage.getItem("auth-token")) {
+      fetch("http://localhost:8000/removefromcart", {
+        method: "POST",
+        headers: {
+          Accept: "application/form-data",
+          "auth-token": `${localStorage.getItem("auth-token")}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ itemId: itemId }),
+      })
+        .then((response) => response.json())
+        .then((data) => setCartItems(data));
+    }
   };
   const updateCartQuantity = (itemId, newQuantity) => {
     setCartItems((prev) => ({ ...prev, [itemId]: newQuantity }));
   };
-  const getTotalCartAmount=()=>{
-    let totalAmout=0;
-    for(const item in cartItems){
-       if(cartItems[item]>0)
-       {
-        let itemInfo=all_product.find((product)=>product.id===Number(item));
-        totalAmout+=itemInfo.new_price*cartItems[item]
-       }
+  const getTotalCartAmount = () => {
+    let totalAmout = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmout += itemInfo.new_price * cartItems[item];
+      }
     }
     return totalAmout;
-
-  }
-  const getTotalCartItems=()=>{
-    let totalItem=0;
-    for(const item in cartItems)
-    {
-      if(cartItems[item]>0){
-        totalItem+=cartItems[item];
-     }
+  };
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
     }
     return totalItem;
-  }
-  const contextValue = { all_product, cartItems, addToCart, removeFromCart,updateCartQuantity,getTotalCartAmount,
-    getTotalCartItems
+  };
+  const contextValue = {
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+    updateCartQuantity,
+    getTotalCartAmount,
+    getTotalCartItems,
   };
 
   return (
